d84806c3fb251df7fabcc671c605d3d8133b7275
Josef Vlach <vlach.josef@gmail.com>
2021-04-08 20:13:03 +0100

    Ability to refresh compact, detailed and proto view of semanticdb

--- scala-semanticdb-mode.el
+++ scala-semanticdb-mode.el
@@ -1,4 +1,5 @@
 ;;; scala-semanticdb-mode.el --- Major mode for viewing scala.semanticdb files
+;;; Copyright (c) 2021 Josef Vlach
 
 (defcustom scala-semanticdb-metap "metap"
   "Program metap executable name."
@@ -7,17 +8,45 @@
 
 (defun scala-semanticdb-run-compact-metap ()
   (interactive)
-  (scala-semanticdb-run-metap 'scala-semanticdb-compact-mode "compact" (buffer-file-name)))
+  (scala-semanticdb--compact-run nil (buffer-file-name)))
 
 (defun scala-semanticdb-run-detailed-metap ()
   (interactive)
-  (scala-semanticdb-run-metap 'scala-semanticdb-detailed-mode "detailed" (buffer-file-name)))
+  (scala-semanticdb--detailed-run nil (buffer-file-name)))
 
 (defun scala-semanticdb-run-proto-metap ()
   (interactive)
-  (scala-semanticdb-run-metap 'scala-semanticdb-proto-mode "proto" (buffer-file-name)))
+  (scala-semanticdb--proto-run nil (buffer-file-name)))
 
-(defun scala-semanticdb-run-metap (minor-mode arg semanticdb-file)
+(defun scala-semanticdb-compact-output ()
+  (interactive)
+  (scala-semanticdb--compact-run))
+
+(defun scala-semanticdb-detailed-output ()
+  (interactive)
+  (scala-semanticdb--detailed-run))
+
+(defun scala-semanticdb-proto-output ()
+  (interactive)
+  (scala-semanticdb--proto-run))
+
+(defun scala-semanticdb-refresh ()
+  (interactive)
+  (cond
+   ((bound-and-true-p scala-semanticdb-compact-mode) (scala-semanticdb--compact-run t))
+   ((bound-and-true-p scala-semanticdb-detailed-mode) (scala-semanticdb--detailed-run t))
+   ((bound-and-true-p scala-semanticdb-proto-mode) (scala-semanticdb--proto-run t))))
+
+(defun scala-semanticdb--compact-run(&optional refresh file)
+  (scala-semanticdb-run-metap 'scala-semanticdb-compact-mode "compact" (or file semanticdb-source-file) refresh))
+
+(defun scala-semanticdb--detailed-run(&optional refresh file)
+  (scala-semanticdb-run-metap 'scala-semanticdb-detailed-mode "detailed" (or file semanticdb-source-file) refresh))
+
+(defun scala-semanticdb--proto-run(&optional refresh file)
+  (scala-semanticdb-run-metap 'scala-semanticdb-proto-mode "proto" (or file semanticdb-source-file) refresh))
+
+(defun scala-semanticdb-run-metap (minor-mode arg semanticdb-file &optional refresh)
   (unless (executable-find scala-semanticdb-metap)
     (error "Could not find %s on PATH. Please install %s program or customize the scala-semanticdb-metap variable."
            scala-semanticdb-metap scala-semanticdb-metap))
@@ -25,21 +54,36 @@
   (let* ((metap-file-name (format "%s.%s.metap" semanticdb-file arg))
          (metap-buffer (file-name-nondirectory metap-file-name)))
 
-    ;; Switch to existing .metap buffer if one exist
+    ;; Switch to existing .metap buffer if one exists
     (if (get-buffer metap-buffer)
-        (switch-to-buffer metap-buffer)
-      (call-process scala-semanticdb-metap nil metap-buffer t (concat "-" arg) semanticdb-file)
-      (with-current-buffer metap-buffer
-        (setq buffer-read-only t)
-        (setq-local semanticdb-source-file semanticdb-file)
-        (funcall minor-mode)
-        (switch-to-buffer (current-buffer))))))
+        (if (not refresh)
+            (switch-to-buffer metap-buffer)
+          (kill-buffer metap-buffer)
+          (scala-semanticdb-call-process metap-buffer arg semanticdb-file))
+      (scala-semanticdb-call-process metap-buffer arg semanticdb-file))))
+
+(defun scala-semanticdb-call-process (metap-buffer arg semanticdb-file)
+  (call-process scala-semanticdb-metap nil metap-buffer t (concat "-" arg) semanticdb-file)
+  (with-current-buffer metap-buffer
+    (setq buffer-read-only t)
+    (setq-local semanticdb-source-file semanticdb-file)
+    (funcall minor-mode)
+    (switch-to-buffer (current-buffer))))
 
 (defvar scala-semanticdb-mode-map
   (let ((map (make-sparse-keymap)))
     (define-key map (kbd "C-c C-c") 'scala-semanticdb-run-compact-metap)
     (define-key map (kbd "C-c C-d") 'scala-semanticdb-run-detailed-metap)
     (define-key map (kbd "C-c C-p") 'scala-semanticdb-run-proto-metap)
+    map)
+  "Local key map used for semanticdb mode")
+
+(defvar scala-semanticdb-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "c") 'scala-semanticdb-compact-output)
+    (define-key map (kbd "d") 'scala-semanticdb-detailed-output)
+    (define-key map (kbd "p") 'scala-semanticdb-proto-output)
+    (define-key map (kbd "g") 'scala-semanticdb-refresh)
     map)
   "Local key map used for semanticdb mode")
 
@@ -52,26 +96,6 @@ When started, runs `scala-semanticdb-mode-hook'.
 \\{semanticdb-mode-map}"
 
   (use-local-map scala-semanticdb-mode-map))
-
-(defun scala-semanticdb-compact-output ()
-  (interactive)
-  (scala-semanticdb-run-metap 'scala-semanticdb-compact-mode "compact" semanticdb-source-file))
-
-(defun scala-semanticdb-detailed-output ()
-  (interactive)
-  (scala-semanticdb-run-metap 'scala-semanticdb-detailed-mode "detailed" semanticdb-source-file))
-
-(defun scala-semanticdb-proto-output ()
-  (interactive)
-  (scala-semanticdb-run-metap 'scala-semanticdb-proto-mode "proto" semanticdb-source-file))
-
-(defvar scala-semanticdb-map
-  (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "c") 'scala-semanticdb-compact-output)
-    (define-key map (kbd "d") 'scala-semanticdb-detailed-output)
-    (define-key map (kbd "p") 'scala-semanticdb-proto-output)
-    map)
-  "Local key map used for semanticdb mode")
 
 (define-minor-mode scala-semanticdb-compact-mode
   "Show detailed output from metap."
