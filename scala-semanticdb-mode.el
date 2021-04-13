86a028f17d1a8b6c7351e8dab80099b1188aaa5b
Josef Vlach <vlach.josef@gmail.com>
2021-04-13 22:35:13 +0100

    Add header-line support, minor modes -> major modes migration

--- scala-semanticdb-mode.el
+++ scala-semanticdb-mode.el
@@ -1,10 +1,81 @@
-;;; scala-semanticdb-mode.el --- Major mode for viewing scala.semanticdb files
+;;; scala-semanticdb-mode.el --- Major mode for viewing scala.semanticdb files -*- lexical-binding: t; -*-
 ;;; Copyright (c) 2021 Josef Vlach
+
+(defconst scala-semanticdb-font-lock-keywords
+  (eval-when-compile
+    `(
+      ("Summary:$" . font-lock-keyword-face)
+      ("Symbols:$" . font-lock-keyword-face)
+      ("Occurrences:$" . font-lock-keyword-face)
+      ("Diagnostics:$" . font-lock-keyword-face)
+      ("\\[[[:digit:]]*:[[:digit:]]*..[[:digit:]]*:[[:digit:]]*) \\(=>\\) .*$" (1 font-lock-keyword-face)) ;; role: DEFINITION
+      ("\\[[[:digit:]]*:[[:digit:]]*..[[:digit:]]*:[[:digit:]]*) \\(<=\\) .*$" (1 font-lock-type-face))    ;; role: REFERENCE
+      ))
+  "Default expressions to highlight in scala-semanticdb derived modes.")
+
+(defun scala-semanticdb-parent-mode ()
+  "Generic mode to derive all other semanticdb buffer modes from."
+  (kill-all-local-variables)
+  (setq buffer-read-only t)
+  (buffer-disable-undo)
+  (add-hook
+   'kill-buffer-hook
+   (function
+    (lambda ()
+      ))
+   nil t))
 
 (defcustom scala-semanticdb-metap "metap"
   "Program metap executable name."
   :type 'string
   :group 'scala-semanticdb)
+
+(defvar-local semanticdb-source-file nil)
+
+;;from gdb-make-header-line-mouse-map
+(defun scala-semanticdb-make-header-line-mouse-map (mouse function) "\
+Return a keymap with single entry for mouse key MOUSE on the header line.
+MOUSE is defined to run function FUNCTION with no args in the buffer
+corresponding to the mode line clicked."
+  (let ((map (make-sparse-keymap)))
+    (define-key map (vector 'header-line mouse) function)
+    (define-key map (vector 'header-line 'down-mouse-1) 'ignore)
+    map))
+
+(defmacro scala-semanticdb-propertize-header (name fn help-echo mouse-face face)
+  `(propertize ,name
+	       'help-echo ,help-echo
+	       'mouse-face ',mouse-face
+	       'face ',face
+	       'local-map
+	       (scala-semanticdb-make-header-line-mouse-map
+	        'mouse-1
+	        (lambda (event) (interactive "e")
+                  (when ,fn (funcall ,fn))))))
+
+(defvar scala-semanticdb-compact-header
+  (list
+   (scala-semanticdb-propertize-header "Compact" nil nil nil mode-line)
+   " "
+   (scala-semanticdb-propertize-header "Detailed" #'scala-semanticdb-detailed-output "mouse-1: select" mode-line-highlight mode-line-inactive)
+   " "
+   (scala-semanticdb-propertize-header "Proto" #'scala-semanticdb-proto-output "mouse-1: select" mode-line-highlight mode-line-inactive)))
+
+(defvar scala-semanticdb-detailed-header
+  (list
+   (scala-semanticdb-propertize-header "Compact" #'scala-semanticdb-compact-output "mouse-1: select" mode-line-highlight mode-line-inactive)
+   " "
+   (scala-semanticdb-propertize-header "Detailed" nil nil nil mode-line)
+   " "
+   (scala-semanticdb-propertize-header "Proto" #'scala-semanticdb-proto-output "mouse-1: select" mode-line-highlight mode-line-inactive)))
+
+(defvar scala-semanticdb-proto-header
+  (list
+   (scala-semanticdb-propertize-header "Compact" #'scala-semanticdb-compact-output "mouse-1: select" mode-line-highlight mode-line-inactive)
+   " "
+   (scala-semanticdb-propertize-header "Detailed" #'scala-semanticdb-detailed-output "mouse-1: select" mode-line-highlight mode-line-inactive)
+   " "
+   (scala-semanticdb-propertize-header "Proto" nil nil nil mode-line)))
 
 (defun scala-semanticdb-run-compact-metap ()
   (interactive)
@@ -32,43 +103,45 @@
 
 (defun scala-semanticdb-refresh ()
   (interactive)
+  (message "Refreshing: %s" (buffer-name))
   (cond
-   ((bound-and-true-p scala-semanticdb-compact-mode) (scala-semanticdb--compact-run t))
-   ((bound-and-true-p scala-semanticdb-detailed-mode) (scala-semanticdb--detailed-run t))
-   ((bound-and-true-p scala-semanticdb-proto-mode) (scala-semanticdb--proto-run t))))
+   ((derived-mode-p 'scala-semanticdb-compact-mode) (scala-semanticdb--compact-run t))
+   ((derived-mode-p 'scala-semanticdb-detailed-mode) (scala-semanticdb--detailed-run t))
+   ((derived-mode-p 'scala-semanticdb-proto-mode) (scala-semanticdb--proto-run t))))
 
 (defun scala-semanticdb--compact-run(&optional refresh file)
-  (scala-semanticdb-run-metap 'scala-semanticdb-compact-mode "compact" (or file semanticdb-source-file) refresh))
+  (scala-semanticdb-run-metap 'scala-semanticdb-compact-mode "compact" (or file semanticdb-source-file) scala-semanticdb-compact-header refresh))
 
 (defun scala-semanticdb--detailed-run(&optional refresh file)
-  (scala-semanticdb-run-metap 'scala-semanticdb-detailed-mode "detailed" (or file semanticdb-source-file) refresh))
+  (scala-semanticdb-run-metap 'scala-semanticdb-detailed-mode "detailed" (or file semanticdb-source-file) scala-semanticdb-detailed-header refresh))
 
 (defun scala-semanticdb--proto-run(&optional refresh file)
-  (scala-semanticdb-run-metap 'scala-semanticdb-proto-mode "proto" (or file semanticdb-source-file) refresh))
+  (scala-semanticdb-run-metap 'scala-semanticdb-proto-mode "proto" (or file semanticdb-source-file) scala-semanticdb-proto-header refresh))
 
-(defun scala-semanticdb-run-metap (minor-mode arg semanticdb-file &optional refresh)
+(defun scala-semanticdb-run-metap (minor-mode arg semanticdb-file header-line &optional refresh)
   (unless (executable-find scala-semanticdb-metap)
     (error "Could not find %s on PATH. Please install %s program or customize the scala-semanticdb-metap variable."
            scala-semanticdb-metap scala-semanticdb-metap))
 
-  (let* ((metap-file-name (format "%s.%s.metap" semanticdb-file arg))
-         (metap-buffer (file-name-nondirectory metap-file-name)))
+  (let ((metap-buffer (format "*metap %s %s*" arg (file-name-nondirectory semanticdb-file))))
 
     ;; Switch to existing .metap buffer if one exists
     (if (get-buffer metap-buffer)
         (if (not refresh)
             (switch-to-buffer metap-buffer)
           (kill-buffer metap-buffer)
-          (scala-semanticdb-call-process metap-buffer arg semanticdb-file))
-      (scala-semanticdb-call-process metap-buffer arg semanticdb-file))))
+          (scala-semanticdb-call-process minor-mode metap-buffer arg semanticdb-file header-line))
+      (scala-semanticdb-call-process minor-mode metap-buffer arg semanticdb-file header-line))))
 
-(defun scala-semanticdb-call-process (metap-buffer arg semanticdb-file)
+(defun scala-semanticdb-call-process (minor-mode metap-buffer arg semanticdb-file header-line)
   (call-process scala-semanticdb-metap nil metap-buffer t (concat "-" arg) semanticdb-file)
   (with-current-buffer metap-buffer
-    (setq buffer-read-only t)
-    (setq-local semanticdb-source-file semanticdb-file)
+    (goto-char (point-min))
     (funcall minor-mode)
-    (switch-to-buffer (current-buffer))))
+    (setq header-line-format header-line)
+    (setq-local semanticdb-source-file semanticdb-file)
+    (switch-to-buffer (current-buffer))
+    (current-buffer)))
 
 (defvar scala-semanticdb-mode-map
   (let ((map (make-sparse-keymap)))
@@ -88,34 +161,49 @@
   "Local key map used for semanticdb mode")
 
 ;;;###autoload
-(define-derived-mode scala-semanticdb-mode special-mode "Semanticdb"
+(define-derived-mode scala-semanticdb-mode scala-semanticdb-parent-mode "Semanticdb"
   "Major mode for viewing scala.semanticdb files.
 
-When started, runs `scala-semanticdb-mode-hook'.
-
-\\{semanticdb-mode-map}"
+When started, runs `scala-semanticdb-mode-hook'."
 
   (use-local-map scala-semanticdb-mode-map))
 
-(define-minor-mode scala-semanticdb-compact-mode
-  "Show detailed output from metap."
-  :lighter " compact"
-  :keymap scala-semanticdb-map)
+;;;###autoload
+(define-derived-mode scala-semanticdb-compact-mode scala-semanticdb-parent-mode "Compact"
+  "Major mode for viewing scala.semanticdb files.
 
-(define-minor-mode scala-semanticdb-detailed-mode
-  "Show detailed output from metap."
-  :lighter " detailed"
-  :keymap scala-semanticdb-map)
+When started, runs `scala-semanticdb-compact-mode-hook'."
 
-(define-minor-mode scala-semanticdb-proto-mode
-  "Show detailed output from metap."
-  :lighter " proto"
-  :keymap scala-semanticdb-map)
+  (use-local-map scala-semanticdb-map)
+  (set (make-local-variable 'font-lock-defaults) '(scala-semanticdb-font-lock-keywords)))
+
+;;;###autoload
+(define-derived-mode scala-semanticdb-detailed-mode scala-semanticdb-parent-mode "Detailed"
+  "Major mode for viewing scala.semanticdb files.
+
+When started, runs `scala-semanticdb-compact-mode-hook'."
+
+  (use-local-map scala-semanticdb-map)
+  (set (make-local-variable 'font-lock-defaults) '(scala-semanticdb-font-lock-keywords)))
+
+;;;###autoload
+(define-derived-mode scala-semanticdb-proto-mode scala-semanticdb-parent-mode "Proto"
+  "Major mode for viewing scala.semanticdb files.
+
+When started, runs `scala-semanticdb-compact-mode-hook'."
+
+  (use-local-map scala-semanticdb-map)
+  (set (make-local-variable 'font-lock-defaults) '(scala-semanticdb-font-lock-keywords)))
 
 ;;;###autoload
 (progn
   (add-to-list 'auto-mode-alist
                '("\\.semanticdb\\'" . scala-semanticdb-mode)))
+
+(add-hook 'scala-semanticdb-mode-hook
+          (lambda (&rest args)
+            (when (string= ".semanticdb" (substring (buffer-file-name) -11 nil))
+              (switch-to-buffer (scala-semanticdb-run-compact-metap)))))
 
 (provide 'scala-semanticdb-mode)
 ;;; scala-semanticdb-mode.el ends here
